b927a70a289fe827e555d49351d6b6081b125280
Unknown <munteanu.nick@gmail.com>
2019-10-13 23:21:22 +1100

    Updated Civic Graph
    + The graphing code library file
    + Sample data file for testing
    + List for Civic's last 24 entries
    > Graph now displays last 24 entries for Civic

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
+var app = angular.module('myApp', ['ngRoute', 'ui.bootstrap','d3']);
 
 console.log("step 1");
 
@@ -16,7 +16,7 @@ app.config(function($routeProvider, $locationProvider) {
 		})
 		.when('/civic', {
 			templateUrl: 'views/civic.html',
-			controller: 'mapController',
+			controller: 'civicCntrl',
 			activetab: "civic"
 		})
 		.when('/florey', {
@@ -44,7 +44,7 @@ app.controller('listController', function($scope, $http) {
 		$scope.aqData = response.data;
 		console.log($scope.aqData);
 	})
-})
+});
 
 app.controller('mapController', function($scope, $rootScope, $compile) {
 	console.log("map page");
@@ -88,4 +88,75 @@ app.controller('mapController', function($scope, $rootScope, $compile) {
 	}
 
 	google.maps.event.addDomListener(window, 'load', initialize);
-})
+});
+
+app.controller('civicCntrl', ['d3', '$scope', '$http', function(d3, $scope, $http){
+
+	$http.get("https://www.data.act.gov.au/resource/94a5-zqnn.json?$limit=24&name='Civic'").then(function(response) {
+		$scope.aqData = response.data;
+
+		$scope.drawBarChart = function(){
+
+		
+			var margin = {top: 20, right: 20, bottom: 20, left: 20},
+				width = 960 - margin.left - margin.right,
+				height = 500 - margin.top - margin.bottom;
+			var x = d3.scaleBand()
+				.rangeRound([0, width], .1)
+					.paddingInner(0.1);
+			var y = d3.scaleLinear()
+				.range([height, 0]);
+			var xAxis = d3.axisBottom()
+				.scale(x)
+			;
+			var yAxis = d3.axisLeft()
+				.scale(y)
+				.ticks(10);
+			var svg = d3.select("#bar-chart").append("svg")
+				.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+			  .append("g")
+				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+			/* d3.tsv("data.tsv", type, function(error, data) {
+				  if (error) throw error; */
+				/*  console.log($scope.aqData); */
+				  data = $scope.aqData; 
+				  console.log(data);
+
+				  x.domain(data.map(function(d) { return d.time; }));
+				  y.domain([0, d3.max(data, function(d) { return d.aqi_site; })]);
+				  svg.append("g")
+					  .attr("class", "x axis")
+					  .attr("transform", "translate(0," + height + ")")
+					  .call(xAxis);
+				svg.append("g")
+					  .attr("class", "y axis")
+					  .call(yAxis)
+					.append("text")
+					  .attr("transform", "rotate(-90)")
+					  .attr("y", 6)
+					  .attr("dy", ".71em")
+					  .style("text-anchor", "end")
+					  .text("aqi_site");
+				svg.selectAll(".bar")
+					.data(data)
+					.enter().append("rect")
+					  .attr("class", "bar")
+					  .attr("x", function(d) { return x(d.time); })
+					  .attr("width", x.bandwidth())
+					  .attr("y", function(d) { return y(d.aqi_site); })
+					  .attr("height", function(d) { return height - y(d.aqi_site); });
+			/* }); */
+	
+			function type(d) {
+			  d.aqi_site = +d.aqi_site;
+			  return d;
+			}
+		}
+	
+		$scope.drawBarChart();
+	})
+	
+
+}]);
